f430c8abbaadd329550fbc7d07229bc96c2d4ec2
Abdelrahman-Elnagar <126884720+Abdelrahman-Elnagar@users.noreply.github.com>
2024-05-13 03:05:34 +0300

    Product browse, search and filter added

--- client/src/Pages/Products/Products.jsx
+++ client/src/Pages/Products/Products.jsx
@@ -1,12 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import "./Products.css"
+
 const Products = () => {
-  return <div>Products</div>;
-  //SARAH ALAA NAGAR
+  const [products, setProducts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filterType, setFilterType] = useState(''); // State for selected filter type
 
-  //code to get all products
+  useEffect(() => {
+    fetch('http://localhost:3000/user/Products')
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setProducts(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching products:', error);
+      });
+  }, []);
 
-  //isGuest && <button for buy redirect to login>
-  //isAdmin && <button for add product and delete product>
-  //isLogged && <button for buy product>
+  const filteredProducts = products.filter(product =>
+    product.Name.toLowerCase().includes(searchTerm.toLowerCase()) &&
+    (filterType === '' || product.Category === filterType) // Apply filter based on selected type
+  );
+
+  const handleFilterChange = (e) => {
+    setFilterType(e.target.value); // Update filter type when dropdown value changes
+  };
+
+  return (
+    <div>
+      <div className="search-filter-container">
+        <input className="search-bar"
+          type="text"
+          placeholder="Search for products ..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <select id="type-filter" value={filterType} onChange={handleFilterChange} className="filter-dropdown">
+          <option value="">All</option>
+          <option value="Apparel">Apparel</option>
+          <option value="Cars">Cars</option>
+          <option value="Accessories">Accessories</option>
+        </select>
+      </div>
+      <div className="products-container">
+        {filteredProducts.map((product) => (
+          <div key={product._id} className="product-box">
+            <div className="product-image-container">
+              <img src={product.Images[0]} alt={product.Name} className="product-image" />
+              <div className="product-info">
+                <p className="product-name">{product.Name}</p>
+                <p className="product-type">Type: {product.Category}</p>
+                <p className="product-price">Price: {product.Price +"$"}</p>
+                <p className="product-stock">Stock: {product.Stock}</p>
+              </div>
+            </div>
+            <div className="product-details">
+              <p className="description-hidden">Description: {product.Description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Products;
